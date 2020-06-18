f0fbb44eac28caf789300b1e3657ff03d2c4df7d
usergithub <breen.78@hotmail.com>
2020-06-18 11:51:20 -0500

    sign in component

--- src/home/home.js
+++ src/home/home.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import pic1 from './pic1.jpeg'
+import React, { useState, useEffect } from 'react'
 
-function View() {
+const FoodItem = ({ item, price, image }) => (
+    <li>{item} - {price} <img src={image}></img></li>
+)
+
+const Food = ({ }) => {
+    const [desayunos, setDesayunos] = useState([]);
+
+    useEffect(() => {
+        const desayunoMenuUrl = 'https://v2-api.sheety.co/08037a6e719e10abd51ee7fe17bba593/burgerQueenMenu/desayunos'
+        fetch(desayunoMenuUrl)
+            .then(res => res.json())
+            .then(data => {
+                //setDesayunos(data.desayunos);
+                setDesayunos(data.desayunos.map((d) => ({
+                    ...d,
+                    image: d.image.replace(/"/ig, ''),
+                })));
+            })
+    }, []);
+
     return (
-        <div className="View">
-            <h1>hola mundo</h1>
-            <img src={pic1} alt='fond0' />
-        </div>
+        <div>
+            <ul>
+                {desayunos.map((d) => (
+                    <FoodItem key={d.item} item={d.item} price={d.price} image={d.image} />
+                ))}
+            </ul>
+        </div >
     );
-}
+};
 
-export default View;
+export default Food;
+
+
