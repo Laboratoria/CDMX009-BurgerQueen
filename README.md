27fb3bcbcdf1861454b607706d9d8de140dc83d0
vanyaxel <vanya_xel@live.com.mx>
2020-06-17 20:56:19 -0500

    fetch

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Burger Queen
+# Burger Queen 
 
 ## Preámbulo
 
---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import pic1 from './pic1.jpeg'
+import style from './style'
+
+function View() {
+    return (
+        <div className="View">
+            <h1>hola mundo</h1>
+            <img src={pic1} alt='fond0' />
+        </div>
+    );
+}
+
+export default View;
