0953900007e2b73d927fd71fe74c1e84654ccc07
IrisFyD <iriis.trejo.c@gmail.com>
2020-06-24 18:30:05 -0500

    brackfast

--- src/components/Breakfast/Breakfast.jsx
+++ src/components/Breakfast/Breakfast.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Breakfast = (props) => {
     const menu = props.food;
-
     const cards = menu.map(elem =>(
         <div>
             <img src={elem.img} alt={elem.name}/>
