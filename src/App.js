4b661b59db6f91b9092679794c6ee982faccb5a4
vanyaxel <vanya_xel@live.com.mx>
2020-06-30 11:59:38 -0500

    order component style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard.jsx';
-import SignIn from './components/auth/SignIn';
-import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
+import Orders from './components/orders/Orders'
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path ='/' component={Dashboard} />
-        <Route path='/signin' component={SignIn} />
-      </Switch>
-    </div>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/prueba' component={Orders} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/orders/Orders.js
@@ -0,0 +1,173 @@
+import React from 'react'
+import pendingIcon from './images/pending.svg';
+import cancelIcon from './images/cancel.svg';
+import readyIcon from './images/ready.svg';
+
+
+function Orders() {
+    return (
+        <div>
+            <div className='row order-h2'>
+                <h2 className='white-text'>Mis ordenes</h2>
+            </div>
+            <div >
+                <div className='col m12 resume-box'>
+                    <div className='post-it'>
+                        <div className='text-order-resume'>
+                            <div className='table-info'>
+                                <p className='table-info'>Mesa 1</p>
+                                <p className='table-info'>10:30 am</p>
+                            </div>
+                            <div>
+                                <div className='col m1'>
+                                    <form action="#">
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className='resume-status'>
+                                <div>
+                                    <img className='status' src={pendingIcon} />
+                                </div>
+                                <p> no pagado</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='post-it'>
+                        <div className='text-order-resume'>
+                            <div className='table-info'>
+                                <p className='table-info'>Mesa 1</p>
+                                <p className='table-info'>10:30 am</p>
+                            </div>
+                            <div>
+                                <div className='col m1'>
+                                    <form action="#">
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className='resume-status'>
+                                <div>
+                                    <img className='status' src={pendingIcon} />
+                                </div>
+                                <p> no pagado</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='post-it'>
+                        <div className='text-order-resume'>
+                            <div className='table-info'>
+                                <p className='table-info'>Mesa 1</p>
+                                <p className='table-info'>10:30 am</p>
+                            </div>
+                            <div>
+                                <div className='col m1'>
+                                    <form action="#">
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className='resume-status'>
+                                <div>
+                                    <img className='status' src={pendingIcon} />
+                                </div>
+                                <p> no pagado</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='post-it'>
+                        <div className='text-order-resume'>
+                            <div className='table-info'>
+                                <p className='table-info'>Mesa 1</p>
+                                <p className='table-info'>10:30 am</p>
+                            </div>
+                            <div>
+                                <div className='col m1'>
+                                    <form action="#">
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                        <p className='chosen-item'>
+                                            <label>
+                                                <input className='box' type="checkbox" />
+                                                <span className='black-text text-item'>Hamburguesa</span>
+                                            </label>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className='resume-status'>
+                                <div>
+                                    <img className='status' src={pendingIcon} />
+                                </div>
+                                <p> no pagado</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </div >
+    )
+}
+
+export default Orders;
