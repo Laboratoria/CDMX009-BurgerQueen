3737d0ec072917c96f10fdf1557f19a2b917443c
Esther Manrique González <esmango0610@gmail.com>
2020-07-01 11:00:44 -0500

    Add folder of components and images styles

---
+++ src/components/BackgLogin/BackgLogin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BurgerLogin from '../../assets/imgs/BurgerLogin1.png';
+import styles from './BackgLogin.module.css'
+
+const BackgLogin = () => (
+    <div className={styles.prueba}>
+        <div className={styles.triangle}>
+        
+     <img className={styles.burgerLogin} src={BurgerLogin} alt=""/>
+    </div>
+
+    </div>
+);
+
+export default BackgLogin;
+
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Logo_BQ from '../../assets/imgs/Logo_BQ.png';
+import styles from './Logo.module.css';
+
+const Logo = () => (
+    <img className={styles.logoLogin} src={Logo_BQ} alt=""/>
+);
+
+export default Logo
