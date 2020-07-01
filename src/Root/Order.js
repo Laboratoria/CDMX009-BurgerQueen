3300ca942d0e7b6460b2a24e05f6179ee5632dc2
EdithOrt <eoc.diseno@gmail.com>
2020-06-30 19:40:19 -0500

    Error with counter total order

--- src/Root/Order.js
+++ src/Root/Order.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
-import Experiment from '../components/experiment/Experiment'
-import PreviewOrder from '../components/PreviewOrder/PreviewOrder'
+import Experiment from '../components/experiment/Experiment';
+import PreviewOrder from '../components/PreviewOrder/PreviewOrder';
 
 function Order(){
 
     const [products, setProducts] = useState (
       [
-        { id:1, name:"Sandwich jamón y queso", category: "desayuno", price:5, 
-          img:"https://cocina-casera.com/wp-content/uploads/2016/11/hamburguesa-queso-receta.jpg",
-        },
-        { id:2, name:"Agua chica", category: "desayuno", price:5, 
-          img:"https://us.123rf.com/450wm/31moonlight31/31moonlight311612/31moonlight31161200130/67846902-salpicaduras-de-agua-en-colores-azules-alrededor-de-una-botella-de-pl%C3%A1stico-azul-con-agua-mineral-bote.jpg?ver=6"
-        }
+        {id:1, name:"Sandwich de jamón y queso", price:5,  img:"https://cutt.ly/eik6Mgu", quantity: 1},
+        {id:2, name:"Agua chica", price:5,  img:"https://cutt.ly/LilqkMQ", quantity: 1},
+        {id:3, name:"Agua mediana", price:7,  img:"https://cutt.ly/7ile1MF", quantity: 1},
+        {id:4, name:"Aros de cebolla", price:5,  img:"https://cutt.ly/iile6sl", quantity: 1},
+        {id:5, name:"Café americano", price:5,  img:"https://cutt.ly/bilry71", quantity: 1},
+        {id:6, name:"Café con leche", price:7,  img:"https://cutt.ly/1ilrhF3", quantity: 1},
+        {id:7, name:"Hamburguesa doble pollo", price:15,  img:"https://cutt.ly/yilrnBm", quantity: 1},
+        {id:8, name:"Hamburguesa doble res", price:15,  img:"aqui", quantity: 1},
+        {id:9, name:"Hamburguesa doble vegetariana", price:15,  img:"aqui", quantity: 1},
+        {id:10, name:"Hamburguesa sencilla pollo", price:10,  img:"aqui", quantity: 1},
+        {id:11, name:"Hamburguesa sencilla res", price:10,  img:"aqui", quantity: 1},
+        {id:12, name:"Hamburguesa sencilla vegetariana", price:10,  img:"aqui", quantity: 1},
+        {id:13, name:"Jugo de frutas", price:7,  img:"aqui", quantity: 1},
+        {id:14, name:"Papas fritas", price:10,  img:"aqui", quantity: 1},
+        {id:15, name:"Refresco chico", price:7,  img:"aqui", quantity: 1},
+        {id:16, name:"refresco mediano", price:10,  img:"aqui", quantity: 1}
       ]
     )
 
--- src/components/experiment/Experiment.js
+++ src/components/experiment/Experiment.js
@@ -1,20 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Experiment = ({food, ordered, addProduct, foods}) => {
     
-    const {name, img, id, price} = food;
+    const {name, img, id, price, quantity} = food;
 
     const SelectProduct = id => {
         const product = foods.filter(elem => elem.id === id)
         addProduct([
             ...ordered,
-            product[0]
+            product[0],
         ]);
     }
 
     const DeleteFood = id => {
         const order = ordered.filter(elem => elem.id !== id)
-        addProduct(order)
+        // SelectProduct(order.id);
+        console.log(id)
+    }
+
+    // const [Counter, SetCounter] = useState(quantity);
+
+
+    const [Data, setData] = useState ({
+        quantityFood: parseFloat(quantity),
+        total: parseFloat(price)*quantityFood,
+        id: id
+    })
+
+    const UpdateState = e => {
+        setData ({
+            ...Data,
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    const {quantityFood, totalFood, idFood}=Data;
+
+    const ChangeQuantityProduct = e => {
+        e.preventDefault();
+        console.log(Data)
     }
 
     return (
@@ -27,11 +51,36 @@ const Experiment = ({food, ordered, addProduct, foods}) => {
                 : 
                     (
                         <Fragment>
-                            <p>{name}</p>
-                            <p>{price}</p>
-                            <button 
-                            onClick={() => DeleteFood(id)}
-                            >Eliminar</button>
+                            <form onSubmit={ChangeQuantityProduct}>
+                                <div>
+                                <input 
+                                    type="number" 
+                                    name="quantityFood"  
+                                    style={{width: '50px', height: '30px'}}
+                                    onChange={UpdateState}
+                                    value={quantityFood}
+                                />
+                                <input 
+                                    type="hidden" 
+                                    name="total" 
+                                    style={{width: '50px'}}
+                                    onChange={UpdateState}
+                                    value={totalFood}
+                                />
+                                <input 
+                                    type="hidden" 
+                                    name="idFood" 
+                                    style={{width: '50px'}}
+                                    onChange={UpdateState}
+                                    value={idFood}
+                                />
+                                </div>
+                                <p>{name}</p>
+                                <p>{price}</p>
+                                <button 
+                                onClick={() => DeleteFood(id)}
+                                >Eliminar</button>
+                            </form>
                         </Fragment>
                     )
                 }
