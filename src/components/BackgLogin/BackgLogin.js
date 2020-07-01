1fb61bd43c23c89785db1805d54bbcb4e0fdc81e
Esther Manrique González <esmango0610@gmail.com>
2020-07-01 18:26:35 -0500

    Add background images Login-Page1

--- src/components/BackgLogin/BackgLogin.js
+++ src/components/BackgLogin/BackgLogin.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import BurgerLogin from '../../assets/imgs/BurgerLogin1.png';
+import Waiter from '../../assets/imgs/waiter.png';
+import HatCheff from '../../assets/imgs/hat-chef.png';
+
 import styles from './BackgLogin.module.css'
 
-const BackgLogin = () => (
-    <div className={styles.prueba}>
-        <div className={styles.triangle}>
-        
-     <img className={styles.burgerLogin} src={BurgerLogin} alt=""/>
-    </div>
 
+const BackgLogin = () => (
+    <div className={styles.containerLogin}>
+        <div className={styles.triangle}>
+            
+            <img className={styles.burgerLogin} src={BurgerLogin} alt=""/>
+
+            <img className={styles.waiter} src={Waiter} alt=""/>
+            <img className={styles.hatCheff} src={HatCheff} alt=""/>        
+        
+        </div>
+ 
     </div>
 );
 
