14671bf2ca9d3f3fe6ab5474d1a6c50a1ed42b33
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-07-06 11:03:16 -0500

    Add changes about Styles BackgLogin.module.css and index.js

--- src/components/BackgLogin/index.js
+++ src/components/BackgLogin/index.js
@@ -4,31 +4,25 @@ import { Link } from 'react-router-dom';
 import styles from './BackgLogin.module.css'
 import Logo from '../Logo/Logo';
 
-
 const BackgLogin = () => (
     <div className={styles.containerLogin}>
         <Logo />
+        <div className={styles.contentWrapper}>
+            <div className={styles.wraperSeccionBottons}>
 
-
-        <div className={styles.azul}>
-            <div className={styles.naranja}>
-
-                <Link to="/waiter" className={styles.ancore}>
+                <Link to="/waiter">
                     <span className={styles.iconWaiter}></span>
                     <button type="button" className={styles.buttonWaiter}>Mesero</button>
                 </Link>
             </div>
 
-            <div className={styles.naranja}>
-                <Link to="/waiter" className={styles.ancore}>
+            <div className={styles.wraperSeccionBottons}>
+                <Link to="/waiter">
                     <span className={styles.iconHatCheff}></span>
                     <button type="button" className={styles.buttonChef}>Cocina</button>
                 </Link>
             </div>
-
         </div>
-
-
     </div>
 );
 
