dc15e5b18c491384939b0678024c15915b476805
Sheiki1 <kirokomerteabesos@gmail.com>
2020-07-14 17:08:16 -0500

    creating the last details of menu elements

---
+++ my-app/src/components/Burguer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+  
+const Burguer =() =>{
+    return(
+        <div>
+            <button className="button is-warning" id="burguerS">Hamburguesa Simple</button>
+            <button className="button is-warning" id="burguerD">Hamburguesa Doble</button>
+        </div>
+    )
+}
+
+export default Burguer
---
+++ my-app/src/components/Cafe.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+  
+const Cafe =() =>{
+    return(
+        <div>
+            <button className="button is-warning" id="cafeAm">Cafe Americano</button>
+            <button className="button is-warning" id="cafeLe">Cafe con Leche</button>
+        </div>
+    )
+}
+
+export default Cafe
