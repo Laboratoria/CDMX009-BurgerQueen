2b86ec1b503a270c99f6859bbc17563a16bd0038
usergithub <breen.78@hotmail.com>
2020-07-28 11:12:26 -0500

    new build folder

---
+++ build/precache-manifest.f4815903f772ec0ec709d273a6aabcff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8999e7bcb92faa785e2a8480ba978e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dba0456301227ffb8aa7",
+    "url": "/static/css/2.f0f662b8.chunk.css"
+  },
+  {
+    "revision": "8a619b10b75450a7207e",
+    "url": "/static/css/main.7b764b35.chunk.css"
+  },
+  {
+    "revision": "dba0456301227ffb8aa7",
+    "url": "/static/js/2.bb538255.chunk.js"
+  },
+  {
+    "revision": "bc3ef0f6d6cdcd0da83f9130afe7f154",
+    "url": "/static/js/2.bb538255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a619b10b75450a7207e",
+    "url": "/static/js/main.3b835007.chunk.js"
+  },
+  {
+    "revision": "aebcdf6bcfd2acc05fbf",
+    "url": "/static/js/runtime-main.aebd6d6c.js"
+  },
+  {
+    "revision": "b1db6d7605f038c993a4cd3674c035ef",
+    "url": "/static/media/barras.b1db6d76.png"
+  },
+  {
+    "revision": "07e4c462a718143ae12468a7abbdd4c2",
+    "url": "/static/media/dashboard-image-blur.07e4c462.PNG"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "385ae813926b8078c1a1d6cc7541a009",
+    "url": "/static/media/initBurger.385ae813.png"
+  },
+  {
+    "revision": "9f2b5b69a83f370768c67190a954c655",
+    "url": "/static/media/logo-receipt.9f2b5b69.png"
+  },
+  {
+    "revision": "1b9382a85415cb7d57e79bd682985505",
+    "url": "/static/media/logo.1b9382a8.svg"
+  }
+]);
